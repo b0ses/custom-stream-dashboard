272d1bef4401bb96a16bcc5d3ac9e455449ca62d
b0ses <b0ses@users.noreply.github.com>
2018-12-30 16:51:42 -0500

    Resolving bugs, moving to grand App, prepping for GroupAlerts

--- src/js/App.js
+++ src/js/App.js
@@ -1,68 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-import api from './helpers/api';
-import CustomAlert from './CustomAlert';
-import Alert from './Alert';
+import Alerts from './Alerts';
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => (
+  <div>
+    <Alerts />
+  </div>
+);
 
-    this.state = {
-      alerts: []
-    };
-    this.customAlert = React.createRef();
-
-    this.refreshAlerts = this.refreshAlerts.bind(this);
-    this.setEditAlert = this.setEditAlert.bind(this);
-    this.setAlerts = this.setAlerts.bind(this);
-  }
-
-  componentDidMount() {
-    this.refreshAlerts();
-  }
-
-  setAlerts(data) {
-    const alerts = [];
-    for (let i = 0; i < data.length; i += 1) {
-      const alert = (<Alert
-        key={i}
-        alertData={data[i]}
-        refreshAlerts={this.refreshAlerts}
-        setEditAlert={this.setEditAlert}
-      />
-      );
-      alerts.push(alert);
-    }
-    this.setState({
-      alerts
-    });
-  }
-
-  setEditAlert(editAlert) {
-    this.customAlert.current.prePopulate(editAlert);
-  }
-
-  refreshAlerts() {
-    api.request('alerts/', null, this.setAlerts);
-  }
-
-  render() {
-    const { alerts } = this.state;
-    return (
-      <div>
-        <div className="saved-alerts">
-          <h3>Alerts</h3>
-          <div className="grid">
-            { alerts }
-          </div>
-        </div>
-        <div className="custom-alert">
-          <h3>New Alert</h3>
-          <CustomAlert ref={this.customAlert} refreshAlerts={this.refreshAlerts} />
-        </div>
-      </div>
-    );
-  }
-}
 export default App;
