eaf29718037849e6d9c417c10dd96f2c2354e742
b0ses <b0ses@users.noreply.github.com>
2019-08-27 22:33:27 -0400

    Updated login for security

--- src/js/Auth.js
+++ src/js/Auth.js
@@ -1,98 +1,65 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
-// import { Receiver } from './Login';
-import { OauthReceiver } from 'react-oauth-flow';
+import api from './helpers/api';
 
-const jwtDecode = require('jwt-decode');
+const queryString = require('query-string');
 const kGlobalConstants = require('./Settings').default;
 
 export function isAuthenticated() {
-  return (localStorage.getItem('id') != null || !kGlobalConstants.LOGIN);
-}
-
-function gatherUserDetails(idToken) {
-  const userDetails = jwtDecode(idToken);
-  const { sub } = userDetails;
-  const emailVerified = userDetails.email_verified;
-  const preferredUsername = userDetails.preferred_username;
-  const { picture } = userDetails;
-  return {
-    id: sub,
-    emailVerified,
-    username: preferredUsername,
-    picture
-  };
-}
-
-function handleError(error) {
-  console.error('An error occured');
-  console.error(error.message);
+  return (localStorage.getItem('loggedIn') != null || !kGlobalConstants.LOGIN);
 }
 
 class Auth extends Component {
   constructor(props) {
     super(props);
-    this.state = { redirectToReferrer: false };
+    this.state = {
+      redirectToReferrer: false,
+      errorMessage: null
+    };
 
-    this.handleSuccess = this.handleSuccess.bind(this);
+    this.validate = this.validate.bind(this);
   }
 
-  async handleSuccess(accessToken, { response, state }) {
-    const refreshToken = response.refresh_token;
-    const idToken = response.id_token;
-    const userDetails = gatherUserDetails(idToken);
-    const { emailVerified } = userDetails;
-    if (emailVerified) {
-      console.log('Successfully authorized');
-      localStorage.setItem('accessToken', accessToken);
-      localStorage.setItem('refreshToken', refreshToken);
-      Object.keys(userDetails).map(key => localStorage.setItem(key, userDetails[key]));
-      this.setState({ redirectToReferrer: true });
-    } else {
-      const error = {
-        message: 'Email not verified'
-      };
-      handleError(error);
-    }
+  validate(authCode) {
+    api.request('auth/login', { code: authCode }).then((resp) => {
+      if (resp.status === 200) {
+        console.log('Successfully authorized');
+        localStorage.setItem('loggedIn', true);
+        this.setState({ redirectToReferrer: true });
+      } else {
+        const errorMessage = resp.data.message;
+        this.setState({ errorMessage });
+      }
+    });
   }
 
   render() {
     const { location } = this.props;
+    const { search } = location;
+    const parsed = queryString.parse(search);
     const { from } = location.state || { from: { pathname: '/' } };
+    const { code } = parsed;
     const { redirectToReferrer } = this.state;
+    const { errorMessage } = this.state;
 
     if (redirectToReferrer) {
       return <Redirect to={from} />;
     }
-
-    return (
-      <OauthReceiver
-        tokenUrl="https://id.twitch.tv/oauth2/token"
-        clientId={kGlobalConstants.CLIENT_ID}
-        clientSecret={kGlobalConstants.CLIENT_SECRET}
-        redirectUri={kGlobalConstants.REDIRECT_URI}
-        onAuthSuccess={this.handleSuccess}
-        onAuthError={handleError}
-        render={({ processing, state, error }) => (
-          <div className="login">
-            {processing && <p>Authorizing...</p>}
-            {error && (
-              <p className="error">
-                An error occured:
-                {error.message}
-              </p>
-            )}
-          </div>
-        )}
-      />
-    );
+    if (errorMessage != null) {
+      return <h4>{errorMessage}</h4>;
+    }
+    if (code != null) {
+      this.validate(code);
+    }
+    return (null);
   }
 }
 
 Auth.propTypes = {
   location: PropTypes.shape({
-    state: PropTypes.string
+    state: PropTypes.string,
+    search: PropTypes.string
   })
 };
 
